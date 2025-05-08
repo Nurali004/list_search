1832028334ddb4354a435acefa835e6614dba897
M A C W I N <mavzurovnurali@gmail.com>
2025-05-08 14:06:56 +0500

    jjj

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,18 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    min_num=data[0]
+    count=0
+    for num in data:
+        if num < min_num:
+            min_num=num
+            count=1
+        elif num ==min_num:
+            count +=1
+
+    return count
+
+
+num=[1,1,1,2,3]
+print(find_min_count(num))
+
