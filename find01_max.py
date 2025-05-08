331152aed0b7183d29ff83d72cfb3b624eabc007
M A C W I N <mavzurovnurali@gmail.com>
2025-05-08 12:25:13 +0500

    sss

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,14 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
+    max_num=data[0]
+    for num in data:
+        if num>max_num:
+            max_num=num
+    return max_num
+
+
+
+num=[1,2,3,4,5,6]
+print(find_max(num))
     
