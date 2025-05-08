644666b70c808fd57083eb1acbbdc2c9b30f9289
M A C W I N <mavzurovnurali@gmail.com>
2025-05-08 13:46:06 +0500

    hhh

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,11 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    max_odd_num=0
+    for num in data:
+        if num%2==0:
+            max_odd_num=num
+    return max_odd_num
+
+num=[1,2,3,4,5,6]
+print(find_max_odd(num))
