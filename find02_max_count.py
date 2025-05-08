5a794b5c995ef3adc007592f1bf5149edfbc96cb
M A C W I N <mavzurovnurali@gmail.com>
2025-05-08 12:41:19 +0500

    fff

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,15 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    max_num=data[0]
+    count=1
+    for num in data[1:]:
+        if num>max_num:
+            max_num=num
+            count=1
+        elif num==max_num:
+            count+=1
+    return count
+
+num=[1,2,3,4,5,5,5]
+print(find_max_count(num))
